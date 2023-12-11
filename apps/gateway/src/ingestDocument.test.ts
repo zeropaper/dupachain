05196a89ca9e9f4010cc3718a074d4e6ed452177
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-12-11 08:51:06 +0100

    work on ingestDocument method

---
+++ apps/gateway/src/ingestDocument.test.ts
@@ -0,0 +1,41 @@
+import { config } from "dotenv";
+import { describe, it, expect, beforeAll } from "vitest";
+
+import { resolve } from "node:path";
+
+config({ path: resolve(__dirname, "../../../.env") });
+
+const runs = [
+  [
+    "html",
+    "<h1>test</h1><h2>test</h2><h3>test</h3><h4>test</h4><h5>test</h5><h6>test</h6><p>test</p>",
+  ],
+  [
+    "markdown",
+    "# test\n## test\n### test\n#### test\n##### test\n###### test\n\ntest",
+  ],
+] as const;
+
+describe.each(["hft", "openai"] as const)(
+  "ingestDocument with %s embeddings",
+  (embeddingType) => {
+    let ingestDocument: any;
+    beforeAll(async () => {
+      ingestDocument = await import("./ingestDocument").then(
+        (m) => m.ingestDocument,
+      );
+    });
+
+    it.each(runs)("ingests a %s document", async (format) => {
+      await expect(
+        ingestDocument({
+          reference: "test",
+          content: "test",
+          metadata: { whatever: "test" },
+          format,
+          embeddingType,
+        }),
+      ).resolves.toBeDefined();
+    });
+  },
+);
--- apps/gateway/src/ingestDocument.ts
+++ apps/gateway/src/ingestDocument.ts
@@ -1,8 +1,9 @@
+// NOTE: the store using HuggingFace Transformers with Supabase/gte-small model for embeddings is cause tests to fail, at least if store is imported here
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { getOpenAIStore } from "./tools/stores/sb-openai";
 import { Json } from "./types";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
 
-export async function ingestDocument(document: {
+export async function ingestDocument(options: {
   content: string;
   metadata:
     | ((string | number | boolean | { [key: string]: Json } | Json[]) &
@@ -17,13 +18,39 @@ export async function ingestDocument(document: {
     | null;
   reference: string;
   format: "html" | "markdown";
+  embeddingType?: "openai" | "hft";
 }) {
-  const { format, content, metadata, reference } = document;
+  const {
+    format,
+    content,
+    metadata,
+    reference,
+    embeddingType = "openai",
+  } = options;
 
-  const splitter = RecursiveCharacterTextSplitter.fromLanguage(format, {
-    chunkSize: 256,
-    chunkOverlap: 20,
-  });
+  let store: SupabaseVectorStore;
+  let splitter: RecursiveCharacterTextSplitter;
+
+  switch (embeddingType) {
+    case "openai":
+      const { getOpenAIStore } = await import("./tools/stores/sb-openai");
+      store = await getOpenAIStore();
+      splitter = RecursiveCharacterTextSplitter.fromLanguage(format, {
+        chunkSize: 1024,
+        chunkOverlap: 60,
+      });
+      break;
+    case "hft":
+      const { getHftStore } = await import("./tools/stores/sb-hft");
+      store = await getHftStore();
+      splitter = RecursiveCharacterTextSplitter.fromLanguage(format, {
+        chunkSize: 256,
+        chunkOverlap: 20,
+      });
+      break;
+    default:
+      throw new Error(`Unknown embedding type ${embeddingType}`);
+  }
 
   const splitDocuments = await splitter.createDocuments(
     [content],
@@ -36,6 +63,5 @@ export async function ingestDocument(document: {
     ],
   );
 
-  const store = await getOpenAIStore();
-  await store.addDocuments(splitDocuments);
+  return store.addDocuments(splitDocuments);
 }
