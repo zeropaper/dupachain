3e8719c6d41da3fc99a9fd3158c10f19d2021330
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-12-18 15:59:26 +0100

    refactor createAPIRouter to include chat retrieval

--- apps/gateway/src/createAPIRouter.ts
+++ apps/gateway/src/createAPIRouter.ts
@@ -3,8 +3,8 @@ import { type Logger } from "pino";
 import { addUserMessage } from "./chats/addUserMessage";
 import { postDocumentBodySchema, postMessageBodySchema } from "./schemas";
 import { ingestDocument } from "./documents/ingestDocument";
-import { createServiceClient } from "./createServiceClient";
 import { createAnonClient } from "./createAnonClient";
+import { ChatsRow } from "./types";
 
 export function createAPIRouter(logger: Logger) {
   const router = express.Router();
@@ -49,7 +49,33 @@ export function createAPIRouter(logger: Logger) {
   router.post("/messages", express.json(), async (req, res, next) => {
     const supabase = createAnonClient();
     const { chat_id, content } = postMessageBodySchema.parse(req.body);
-    const insertMessage = await addUserMessage(supabase, { chat_id, content });
+    const chat = await supabase
+      .from("chats")
+      .select()
+      .eq("id", chat_id)
+      .single()
+      .then(({ data, error }) => {
+        if (error) {
+          logger.error({
+            message: "Chat retrieval error",
+            error,
+          });
+          throw new Error("Chat retrieval error");
+        }
+        if (!data) {
+          logger.error({
+            message: "Chat not found",
+          });
+          throw new Error("Chat not found");
+        }
+        return data as ChatsRow;
+      });
+    const insertMessage = await addUserMessage(
+      supabase,
+      { chat_id, content },
+      chat,
+      logger,
+    );
 
     if (insertMessage.error) {
       logger.error({
