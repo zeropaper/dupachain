f98afa9f34e28dc49e45567fd056ee0740b28170
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-12-19 08:38:41 +0100

    move getTesterCall util out of test file

--- apps/gateway/src/runtime.test.ts
+++ apps/gateway/src/runtime.test.ts
@@ -1,38 +1,13 @@
 import { config } from "dotenv";
 import { resolve } from "node:path";
-import { describe, it, expect, beforeAll, beforeEach, vi } from "vitest";
+import { describe, it, expect, beforeAll } from "vitest";
 
 import { getClient, ChatMessageInfo, ChatClient } from "@local/client";
-import OpenAI from "openai";
-import { Agent } from "./types";
 import { loadAgent } from "./loadAgent";
+
 config({ path: resolve(__dirname, "../../../.env") });
-const openai = new OpenAI();
 
-const TESTER_PROMPT_PREFIX = `You are a QA assistant very skilled at impersonating fictional people and use all your skills test a chatbot based on a scenario.
-
-Your messages are only a valid JSON object having the following properties:
-- goalMet: whether the goal of your scenario is met
-- message: the message you send to the chatbot
-- reasoning: your reasoning for sending this message
-
-Here is the scenario you have to follow:`;
-
-async function getTesterCall(
-  persona: string,
-  messages: Pick<ChatMessageInfo, "content" | "role">[] = [],
-) {
-  const prompt = `${TESTER_PROMPT_PREFIX}\n${persona}\n`;
-  const result = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-1106",
-    response_format: { type: "json_object" },
-    messages: [
-      { role: "system", content: prompt },
-      ...messages.map((msg) => ({ role: msg.role, content: msg.content })),
-    ],
-  });
-  return JSON.parse(result.choices[0].message.content || "null");
-}
+import { getTesterCall } from "./getTesterCall";
 
 const agents = {
   default: loadAgent("default"),
