00dfd9eeaa1e07fb714f13ade7c429416acf7aaa
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-12-20 10:53:49 +0100

    move createEvalCallbacks function out of eval index

---
+++ apps/gateway/src/eval/createEvalCallbacks.ts
@@ -0,0 +1,159 @@
+import { CallbackHandlerMethods } from "langchain/callbacks";
+import { LogItems } from "./types";
+
+export async function createEvalCallbacks(): Promise<{
+  teardown: () => Promise<LogItems>;
+  handlers: CallbackHandlerMethods;
+}> {
+  const items: LogItems = [];
+  function write(eventName: string, ...args: any[]) {
+    items.push([Date.now(), eventName, ...args]);
+  }
+  return {
+    async teardown() {
+      return items;
+    },
+    handlers: {
+      handleAgentAction(action, runId, parentRunId, tags) {
+        write("handleAgentAction", action, runId, parentRunId, tags);
+      },
+      handleAgentEnd(action, runId, parentRunId, tags) {
+        write("handleAgentEnd", action, runId, parentRunId, tags);
+      },
+      handleChainEnd(outputs, runId, parentRunId, tags, kwargs) {
+        write("handleChainEnd", outputs, runId, parentRunId, tags, kwargs);
+      },
+      handleChainError(err, runId, parentRunId, tags, kwargs) {
+        write("handleChainError", err, runId, parentRunId, tags, kwargs);
+      },
+      handleChainStart(
+        chain,
+        inputs,
+        runId,
+        parentRunId,
+        tags,
+        metadata,
+        runType,
+        name,
+      ) {
+        write(
+          "handleChainStart",
+          chain,
+          inputs,
+          runId,
+          parentRunId,
+          tags,
+          metadata,
+          runType,
+          name,
+        );
+      },
+      handleChatModelStart(
+        llm,
+        messages,
+        runId,
+        parentRunId,
+        extraParams,
+        tags,
+        metadata,
+        name,
+      ) {
+        write(
+          "handleChatModelStart",
+          llm,
+          messages,
+          runId,
+          parentRunId,
+          extraParams,
+          tags,
+          metadata,
+          name,
+        );
+      },
+      handleLLMEnd(output, runId, parentRunId, tags) {
+        write("handleLLMEnd", output, runId, parentRunId, tags);
+      },
+      handleLLMError(err, runId, parentRunId, tags) {
+        write("handleLLMError", err, runId, parentRunId, tags);
+      },
+      handleLLMNewToken(token, idx, runId, parentRunId, tags, fields) {
+        write(
+          "handleLLMNewToken",
+          token,
+          idx,
+          runId,
+          parentRunId,
+          tags,
+          fields,
+        );
+      },
+      handleLLMStart(
+        llm,
+        prompts,
+        runId,
+        parentRunId,
+        extraParams,
+        tags,
+        metadata,
+        name,
+      ) {
+        write(
+          "handleLLMStart",
+          llm,
+          prompts,
+          runId,
+          parentRunId,
+          extraParams,
+          tags,
+        );
+      },
+      handleRetrieverEnd(documents, runId, parentRunId, tags) {
+        write("handleRetrieverEnd", documents, runId, parentRunId, tags);
+      },
+      handleRetrieverError(err, runId, parentRunId, tags) {
+        write("handleRetrieverError", err, runId, parentRunId, tags);
+      },
+      handleRetrieverStart(
+        retriever,
+        query,
+        runId,
+        parentRunId,
+        tags,
+        metadata,
+        name,
+      ) {
+        write(
+          "handleRetrieverStart",
+          retriever,
+          query,
+          runId,
+          parentRunId,
+          tags,
+          metadata,
+          name,
+        );
+      },
+      handleText(text, runId, parentRunId, tags) {
+        write("handleText", text, runId, parentRunId, tags);
+      },
+      handleToolEnd(output, runId, parentRunId, tags) {
+        write("handleToolEnd", output, runId, parentRunId, tags);
+      },
+      handleToolError(err, runId, parentRunId, tags) {
+        write("handleToolError", err, runId, parentRunId, tags);
+      },
+      handleToolStart(tool, input, runId, parentRunId, tags, metadata, name) {
+        write(
+          "handleToolStart",
+          tool,
+          input,
+          runId,
+          parentRunId,
+          tags,
+          metadata,
+          name,
+        );
+      },
+    },
+  };
+}
--- apps/gateway/src/eval/index.ts
+++ apps/gateway/src/eval/index.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { readFile, writeFile } from "fs/promises";
 import { resolve } from "path";
-import { CallbackHandlerMethods, Callbacks } from "langchain/callbacks";
+import { Callbacks } from "langchain/callbacks";
 import { EvalMessage, runPersona } from "./runPersona";
 import { LocalFileCache } from "langchain/cache/file_system";
 import CallbackHandler from "langfuse-langchain";
@@ -9,165 +9,9 @@ import { prepareTools } from "./prepareTools";
 import { loadEvalFile, defaultRoot } from "./loadEvalFile";
 import { isRunnerWithToolsInfo } from "./type-guards";
 import { LogItems, ChainRunner, ToolsMap } from "./types";
+import { createEvalCallbacks } from "./createEvalCallbacks";
 
 config({ path: resolve(__dirname, "../../../../.env") });
-
-async function createEvalCallbacks(): Promise<{
-  teardown: () => Promise<LogItems>;
-  handlers: CallbackHandlerMethods;
-}> {
-  const items: LogItems = [];
-  function write(eventName: string, ...args: any[]) {
-    items.push([Date.now(), eventName, ...args]);
-  }
-  return {
-    async teardown() {
-      return items;
-    },
-    handlers: {
-      handleAgentAction(action, runId, parentRunId, tags) {
-        write("handleAgentAction", action, runId, parentRunId, tags);
-      },
-      handleAgentEnd(action, runId, parentRunId, tags) {
-        write("handleAgentEnd", action, runId, parentRunId, tags);
-      },
-      handleChainEnd(outputs, runId, parentRunId, tags, kwargs) {
-        write("handleChainEnd", outputs, runId, parentRunId, tags, kwargs);
-      },
-      handleChainError(err, runId, parentRunId, tags, kwargs) {
-        write("handleChainError", err, runId, parentRunId, tags, kwargs);
-      },
-      handleChainStart(
-        chain,
-        inputs,
-        runId,
-        parentRunId,
-        tags,
-        metadata,
-        runType,
-        name,
-      ) {
-        write(
-          "handleChainStart",
-          chain,
-          inputs,
-          runId,
-          parentRunId,
-          tags,
-          metadata,
-          runType,
-          name,
-        );
-      },
-      handleChatModelStart(
-        llm,
-        messages,
-        runId,
-        parentRunId,
-        extraParams,
-        tags,
-        metadata,
-        name,
-      ) {
-        write(
-          "handleChatModelStart",
-          llm,
-          messages,
-          runId,
-          parentRunId,
-          extraParams,
-          tags,
-          metadata,
-          name,
-        );
-      },
-      handleLLMEnd(output, runId, parentRunId, tags) {
-        write("handleLLMEnd", output, runId, parentRunId, tags);
-      },
-      handleLLMError(err, runId, parentRunId, tags) {
-        write("handleLLMError", err, runId, parentRunId, tags);
-      },
-      handleLLMNewToken(token, idx, runId, parentRunId, tags, fields) {
-        write(
-          "handleLLMNewToken",
-          token,
-          idx,
-          runId,
-          parentRunId,
-          tags,
-          fields,
-        );
-      },
-      handleLLMStart(
-        llm,
-        prompts,
-        runId,
-        parentRunId,
-        extraParams,
-        tags,
-        metadata,
-        name,
-      ) {
-        write(
-          "handleLLMStart",
-          llm,
-          prompts,
-          runId,
-          parentRunId,
-          extraParams,
-          tags,
-        );
-      },
-      handleRetrieverEnd(documents, runId, parentRunId, tags) {
-        write("handleRetrieverEnd", documents, runId, parentRunId, tags);
-      },
-      handleRetrieverError(err, runId, parentRunId, tags) {
-        write("handleRetrieverError", err, runId, parentRunId, tags);
-      },
-      handleRetrieverStart(
-        retriever,
-        query,
-        runId,
-        parentRunId,
-        tags,
-        metadata,
-        name,
-      ) {
-        write(
-          "handleRetrieverStart",
-          retriever,
-          query,
-          runId,
-          parentRunId,
-          tags,
-          metadata,
-          name,
-        );
-      },
-      handleText(text, runId, parentRunId, tags) {
-        write("handleText", text, runId, parentRunId, tags);
-      },
-      handleToolEnd(output, runId, parentRunId, tags) {
-        write("handleToolEnd", output, runId, parentRunId, tags);
-      },
-      handleToolError(err, runId, parentRunId, tags) {
-        write("handleToolError", err, runId, parentRunId, tags);
-      },
-      handleToolStart(tool, input, runId, parentRunId, tags, metadata, name) {
-        write(
-          "handleToolStart",
-          tool,
-          input,
-          runId,
-          parentRunId,
-          tags,
-          metadata,
-          name,
-        );
-      },
-    },
-  };
-}
 
 async function main() {
   const serviceClient = await import("../createServiceClient").then(
