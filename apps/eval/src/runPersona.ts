c8e68fa1820d5fb47e6e360b7431ea9829e5d000
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-12-28 17:45:06 +0100

    wrap runPersona content in a try catch

--- apps/eval/src/runPersona.ts
+++ apps/eval/src/runPersona.ts
@@ -38,80 +38,84 @@ export async function runPersona({
   systemPrompt: string;
   cache?: BaseCache;
 }): Promise<{ messages: EvalMessage[]; log: LogItems }> {
-  const { profile, firstMessage, maxCalls } = persona;
-  // in order to ease the reading/organization of data in/with langfuse
-  // we create 3 different sessions
-  const testerCallbacks = await prepareCallbacks(`${runId} tester`);
-  const goalTesterCallbacks = await prepareCallbacks(`${runId} goal tester`);
-  const agentCallbacks = await prepareCallbacks(`${runId} agent`);
-
   const messages: EvalMessage[] = [];
 
-  for (let i = 0; i < maxCalls; i += 1) {
-    const input = await getTesterCall({
-      profile,
-      firstMessage,
-      messages,
-      callbacks: testerCallbacks.callbacks,
-      cache,
-    });
-    messages.push({
-      role: "assistant",
-      content: input.message,
-    });
+  try {
+    const { profile, firstMessage, maxCalls } = persona;
+    // in order to ease the reading/organization of data in/with langfuse
+    // we create 3 different sessions
+    const testerCallbacks = await prepareCallbacks(`${runId} tester`);
+    const goalTesterCallbacks = await prepareCallbacks(`${runId} goal tester`);
+    const agentCallbacks = await prepareCallbacks(`${runId} agent`);
 
-    log.blue("test bot (%s / %s):\n\t%s", i, maxCalls, input.message);
-
-    const output = await runChain({
-      tools: Object.values(toolsMap),
-      systemPrompt,
-      chatMessages: [
-        ...messages,
-        // { role: "assistant", content: "..." }
-      ].map(({ role, ...rest }) => ({
-        ...rest,
-        role: role === "assistant" ? "user" : "assistant",
-      })) as any,
-      callbacks: agentCallbacks.callbacks,
-    });
-
-    messages.push({
-      role: "user",
-      content: output!,
-    });
-    log.cyan("chat bot (%s / %s):\n\t%s", i, maxCalls, output);
-
-    if (persona.goal && messages.length > 1) {
-      const goalMet = await testGoal({
-        callbacks: goalTesterCallbacks.callbacks,
-        cache,
-        persona,
+    for (let i = 0; i < maxCalls; i += 1) {
+      const input = await getTesterCall({
+        profile,
+        firstMessage,
         messages,
+        callbacks: testerCallbacks.callbacks,
+        cache,
       });
-      log[goalMet ? "green" : "magenta"](
-        "goal met? %s\n\t%s",
-        goalMet,
-        persona.goal,
-      );
-      if (goalMet) {
-        break;
+      messages.push({
+        role: "assistant",
+        content: input.message,
+      });
+
+      log.blue("test bot (%s / %s):\n\t%s", i, maxCalls, input.message);
+
+      const output = await runChain({
+        tools: Object.values(toolsMap),
+        systemPrompt,
+        chatMessages: messages.map(({ role, ...rest }) => ({
+          ...rest,
+          role: role === "assistant" ? "user" : "assistant",
+        })) as any,
+        callbacks: agentCallbacks.callbacks,
+      });
+
+      messages.push({
+        role: "user",
+        content: output!,
+      });
+      log.cyan("chat bot (%s / %s):\n\t%s", i, maxCalls, output);
+
+      if (persona.goal && messages.length > 1) {
+        const goalMet = await testGoal({
+          callbacks: goalTesterCallbacks.callbacks,
+          cache,
+          persona,
+          messages,
+        });
+        log[goalMet ? "green" : "magenta"](
+          "goal met? %s\n\t%s",
+          goalMet,
+          persona.goal,
+        );
+        if (goalMet) {
+          break;
+        }
       }
     }
+    // teardown the langfuse managers and get the results
+    const logResults = await Promise.allSettled([
+      testerCallbacks.teardown(),
+      goalTesterCallbacks.teardown(),
+      agentCallbacks.teardown(),
+    ]);
+    return {
+      messages,
+      log: logResults
+        .map((r) =>
+          r.status === "fulfilled"
+            ? r.value
+            : ([[Date.now(), runId, "error", r.reason]] satisfies LogItems),
+        )
+        .reduce((acc, val) => acc.concat(val), []),
+    };
+  } catch (error) {
+    return {
+      messages,
+      log: [[Date.now(), runId, "error", error]],
+    };
   }
-  // teardown the langfuse managers and get the results
-  const logResults = await Promise.allSettled([
-    testerCallbacks.teardown(),
-    goalTesterCallbacks.teardown(),
-    agentCallbacks.teardown(),
-  ]);
-  return {
-    messages,
-    log: logResults
-      .map((r) =>
-        r.status === "fulfilled"
-          ? r.value
-          : ([[Date.now(), runId, "error", r.reason]] satisfies LogItems),
-      )
-      .reduce((acc, val) => acc.concat(val), []),
-  };
 }
